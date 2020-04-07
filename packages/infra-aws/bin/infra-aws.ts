9d477547fead3b23b655501bfb4252a42a0255dd
Yusuke Wada <wada.yusuke@classmethod.jp>
2020-04-07 18:05:13 +0900

    codepipeline

--- packages/infra-aws/bin/infra-aws.ts
+++ packages/infra-aws/bin/infra-aws.ts
@@ -2,12 +2,16 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { greetingServiceApplicationStack } from '../lib/greeting-service-stack';
+import { greetingDeployPipelineStack } from '../lib/pipeline-deploy-stack';
 
 async function buildApp(): Promise<void> {
     const app = new cdk.App();
 
     // Application stack
     await greetingServiceApplicationStack(app, 'StampServiceStack');
+
+    // Deploy stack
+    await greetingDeployPipelineStack(app, 'DeployStack');
 }
 
 buildApp();
